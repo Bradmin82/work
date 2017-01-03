22673f3524fd44f89a98bfa06b66cc0609ff0874
bradmin82 <bweldy82@gmail.com>
2017-01-02 18:03:04 -0800

    auth added, no protected pages

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,15 @@ Route::get('/', function () {
 })->middleware('web');
 
 Route::group(['middleware' => ['web']], function() {
+	//Authentication Routes
+	Route::get('auth/login', 'Auth\LoginController@showLoginForm');
+	Route::post('auth/login', 'Auth\LoginController@login');
+	Route::get('auth/logout', 'Auth\LoginController@logout');
+
+	//Registration Routes
+	Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm');
+	//Route::post('auth/register', 'Auth\RegisterController@register');
+
 	Route::get('blog/{slug}', ['uses' => 'BlogController@getSingle', 'as' => 'blog.single']);
 	Route::get('blog', ['uses' => 'BlogController@getIndex', 'as' => 'blog.index']);
 	Route::get('/', 'PagesController@home');
